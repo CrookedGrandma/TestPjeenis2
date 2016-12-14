0034caf99ad292e1aeb123331fa73305826e09ae
Samuwhale <sfspithorst@gmail.com>
2016-12-14 14:06:14 +0100

    BG color kan veranderd worden vanuit script

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Environment : MonoBehaviour {
+    public Color BGcolor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Camera>().backgroundColor = BGcolor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
