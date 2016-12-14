1578edcd7a0538b22da3aba5f45dafad172de8b9
Samuwhale <sfspithorst@gmail.com>
2016-12-14 14:36:49 +0100

    bijna niets veranderd maar SUPER belangrijke code

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class Environment : MonoBehaviour {
-    public Color BGcolor = Color.white;
+    public Color Desert = Color.white;
+    public Color Sea = Color.white;
+    public Color Forest = Color.white;
 
-	// Use this for initialization
-	void Start () {
-        GetComponent<Camera>().backgroundColor = BGcolor;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Camera>().backgroundColor = Desert;
 	}
 	
 	// Update is called once per frame
