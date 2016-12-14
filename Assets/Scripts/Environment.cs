0fc78f3842237403d517bccba648a547e2133b13
Samuwhale <sfspithorst@gmail.com>
2016-12-14 14:53:15 +0100

    switch case in environment

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Environment : MonoBehaviour {
+public class Environment : MonoBehaviour
+{
     public Color Desert = Color.white;
     public Color Sea = Color.white;
     public Color Forest = Color.white;
+    //set these colors in Unity Editor, replace with backdrop sprites later on
 
-
-    // Use this for initialization
-    void Start () {
-        GetComponent<Camera>().backgroundColor = Desert;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void changebg(string bgcolor)
+    {
+        switch (bgcolor)
+        {
+            case "Desert": GetComponent<Camera>().backgroundColor = Desert; break;
+            case "Sea": GetComponent<Camera>().backgroundColor = Sea; break;
+            case "Forest": GetComponent<Camera>().backgroundColor = Forest; break;
+            default: GetComponent<Camera>().backgroundColor = Color.black; break;
+        }
+    }
 }
