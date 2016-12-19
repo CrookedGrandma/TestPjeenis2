ead925cad956a024bc2a52d0e81c8ba25dfdd457
Samuwhale <sfspithorst@gmail.com>
2016-12-19 14:33:40 +0100

    scale modifiers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,16 +23,18 @@ public class Enemy {
     /// Load an enemy with two attacks
     /// </summary>
     public Enemy(int id, string title, string type, int hp, int strength, string attack1title, int attack1damage, double attack1scaling, string attack2title, int attack2damage, double attack2scaling) {
+        double add1 = (double)strength * attack1scaling; // additional damage for ability 1, based on scaling% multiplied by enemy strength 
+        double add2 = (double)strength * attack2scaling; // additional damage for ability 2, based on scaling% multiplied by enemy strength
         this.ID = id;
         this.Title = title;
         this.Type = type;
         this.HP = hp;
         this.Strength = strength;
         this.Attack1Title = attack1title;
-        this.Attack1Damage = attack1damage;
+        this.Attack1Damage = attack1damage + (int)add1; // damage + additional damage for ability 1
         this.Attack1Scaling = attack1scaling;
         this.Attack2Title = attack2title;
-        this.Attack2Damage = attack2damage;
+        this.Attack2Damage = attack2damage + (int)add2; // damage + additional damage for ability 2
         this.Attack2Scaling = attack2scaling;
         this.Attack3Title = null;
         this.Attack3Damage = 0;
