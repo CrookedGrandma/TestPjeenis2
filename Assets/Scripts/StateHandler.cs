fc295e746d71c5fccbcd4654c608af2dee92e72c
CrookedGrandma <k.t.noot@live.nl>
2016-12-19 13:42:58 +0100

    Aanvalselectie (zie description)
    Enemy kiest welke aanval hij gebruikt op basis van percentages die
    bestaan uit {[damage van aanval]/[damage van alle aanvallen bij elkaar]}

--- Assets/Scripts/StateHandler.cs
+++ Assets/Scripts/StateHandler.cs
@@ -7,6 +7,7 @@ public class StateHandler : MonoBehaviour {
     States state;
     bool ranOnce = false;
     double flashTimer = 0.0;
+    int attNum = 0;
 
     public EnemyChooser enemyChooser;
 
@@ -30,9 +31,8 @@ public class StateHandler : MonoBehaviour {
         flashTimer = Mathf.PingPong(Time.time * 2, 1);
 
         if (ranOnce) {
-            int attackNum = GetAttack();
             if (flashTimer >= 0.9) {
-                if (attackNum == 1) {
+                if (attNum == 1) {
                     enemyChooser.enemyStat.text = "HP: " + e.HP + "\n" +
                                                   "Strength: " + e.Strength + "\n" +
                                                   "<color=red>Attack 1: " + e.Attack1Title + "\n" +
@@ -40,7 +40,7 @@ public class StateHandler : MonoBehaviour {
                                                   "Attack 2: " + e.Attack2Title + "\n" +
                                                   "Damage: " + e.Attack2Damage;
                 }
-                if (attackNum == 2) {
+                if (attNum == 2) {
                     enemyChooser.enemyStat.text = "HP: " + e.HP + "\n" +
                                                   "Strength: " + e.Strength + "\n" +
                                                   "Attack 1: " + e.Attack1Title + "\n" +
@@ -64,14 +64,21 @@ public class StateHandler : MonoBehaviour {
         if (!ranOnce) {
             e = enemyChooser.currentEnemy.linkedEnemy;
             ranOnce = true;
+            attNum = GetAttack();
         }
     }
 
     int GetAttack() {
-        int attackNum = 1;
-        int attackDam = e.Attack1Damage;
-        if (e.Attack2Damage > attackDam) { attackDam = e.Attack2Damage; attackNum = 2; }
-        if (e.Attack3Damage > attackDam) { attackNum = 3; }
+        int attackNum = 0;
+        int a1d = e.Attack1Damage;
+        int a2d = e.Attack2Damage;
+        int a3d = e.Attack3Damage;
+        int totalDam = a1d + a2d + a3d;
+
+        int rndAtt = Random.Range(0, totalDam);
+        if (rndAtt <= a1d) { attackNum = 1; }
+        else if (rndAtt <= a1d + a2d) { attackNum = 2; }
+        else { attackNum = 3; }
 
         return attackNum;
     }
